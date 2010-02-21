bac72b1b85ccbbe37c413630604607c93536d5ca
thisisfranciswu <thisisfranciswu@gmail.com>
2010-02-21 17:59:25 -0500

    Improved padding and corrections to the INSTALL.html file.

--- browser_fiche.js
+++ browser_fiche.js
@@ -1,5 +1,5 @@
-// javascript:bStyle%20=%20document.getElementsByTagName(%22body%22)[0].style;bStyle.width%20=%20%22120%%22;bStyle.height%20=%20%22120%%22;bStyle.padding%20=%20%2210%%22;
+// javascript:bStyle%20=%20document.getElementsByTagName(%22body%22)[0].style;bStyle.width%20=%20%22120%%22;bStyle.height%20=%20%22140%%22;bStyle.padding%20=%20%2210%%2010%%2030%%2010%%22;
 bStyle = document.getElementsByTagName("body")[0].style;
 bStyle.width = "120%";
-bStyle.height = "120%";
-bStyle.padding = "10%";
+bStyle.height = "140%";
+bStyle.padding = "10% 10% 30% 10%";
