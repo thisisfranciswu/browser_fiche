b6d54556d02e218e581539e9291f0092f11f3c87
thisisfranciswu <thisisfranciswu@gmail.com>
2010-02-21 17:40:04 -0500

    Added the bookmarklet to the README file.

--- browser_fiche.js
+++ browser_fiche.js
@@ -1,3 +1,4 @@
+// javascript:bStyle%20=%20document.getElementsByTagName(%22body%22)[0].style;bStyle.width%20=%20%22120%%22;bStyle.height%20=%20%22120%%22;bStyle.padding%20=%20%2210%%22;
 bStyle = document.getElementsByTagName("body")[0].style;
 bStyle.width = "120%";
 bStyle.height = "120%";
