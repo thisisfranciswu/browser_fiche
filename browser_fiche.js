32be53b278a17e93fa551785e8f9478be5314a91
thisisfranciswu <thisisfranciswu@gmail.com>
2010-06-30 00:20:18 -0400

    Finally got bookmarklet working.

--- browser_fiche.js
+++ browser_fiche.js
@@ -1,5 +1,7 @@
-// javascript:bStyle%20=%20document.getElementsByTagName(%22body%22)[0].style;bStyle.width%20=%20%22120%%22;bStyle.height%20=%20%22140%%22;bStyle.padding%20=%20%2210%%2010%%2030%%2010%%22;
-bStyle = document.getElementsByTagName("body")[0].style;
-bStyle.width = "120%";
-bStyle.height = "140%";
-bStyle.padding = "10% 10% 30% 10%";
+// javascript:(function()%20{bStyle%20=%20document.getElementsByTagName(%22body%22)[0].style;bStyle.width%20=%20%22120%%22;bStyle.height%20=%20%22140%%22;bStyle.padding%20=%20%2210%%2010%%2030%%2010%%22;})();
+(function() {
+  bStyle = document.getElementsByTagName("body")[0].style;
+  bStyle.width = "120%";
+  bStyle.height = "140%";
+  bStyle.padding = "10% 10% 30% 10%";
+})();
